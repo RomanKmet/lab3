6e8637e2ceec4f83bf55c2a9439465531d64652d
Roman <romankmet10@gmail.com>
2023-04-14 10:12:07 +0300

    changed task3

--- tasks/src/task3.java
+++ tasks/src/task3.java
@@ -1,8 +1,7 @@
 public class task3 {
     public static void main(String[] args){
-        for(float a=1; a<=10; a++){
-            float b = 10 / a;
-            if (Math.round(b) == b) {
+        for(int a=1; a<=10; a++){
+            if (10%a==0){
                 System.out.println(a);
             }
         }
